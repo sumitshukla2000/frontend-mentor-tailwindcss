da2bfdc02ede3da454a5ba2702b55aad661a72fe
sumitshukla2000 <shuklasumit93@gmail.com>
2023-09-28 19:39:55 +0530

    added js file for hamburger menu

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector("#menu");
+
+hamburger.addEventListener("click" , ()=>{
+    hamburger.classList.toggle("open");
+    // menu.classList.toggle("flex");
+    menu.classList.toggle("hidden");
+});
